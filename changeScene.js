16ae0037d72028ed2865931aba8f8da4b7de68df
Bradford Nelson <b3nelly@hotmail.com>
2023-04-06 04:20:07 -0500

    Updates for @b3nelly/change-obs-scene v1.3

--- changeScene.js
+++ changeScene.js
@@ -1,15 +1,15 @@
 import * as dotenv from "dotenv"; // https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import obsSwitchScenes from "@b3nelly/change-obs-scene"; // https://github.com/web3nelly/change-obs-scene
+import changeScene from "@b3nelly/change-obs-scene"; // https://github.com/web3nelly/change-obs-scene
 https: dotenv.config();
 
 const obsSkipScenes = ["Scene 1"];
 const intervalInSeconds = process.env.OBS_SCENE_INT_SECS ?? 45;
-const obsWebSocketServerURL = process.env.OBS_WS ?? "ws://localhost:4455";
 const obsWebSocketServerPassword = process.env.OBS_WS_PASS ?? undefined;
+const obsWebSocketServerURL = process.env.OBS_WS ?? "ws://localhost:4455";
 
-obsSwitchScenes(
+changeScene(
   intervalInSeconds,
-  obsWebSocketServerURL,
+  obsSkipScenes,
   obsWebSocketServerPassword,
-  obsSkipScenes
+  obsWebSocketServerURL
 );
